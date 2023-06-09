7ff6b00cce74795749c395d47929fe5e73ca51c3
Gabriel Nascimento <135087316+gabrielgnsilva@users.noreply.github.co>
2023-06-09 01:00:23 -0300

    Updated syntax

---
+++ utils/_hasWritePermission.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+#=======================================================================
+# HEADER
+#=======================================================================
+# NAME
+#       Write Permission
+#
+#=======================================================================
+# SYNOPSIS
+#       hasWritePermission FILE...
+#
+#=======================================================================
+# DESCRIPTION
+#       This script is designed to be sourced and provides a reusable
+#       function that checks if the user has permission to write on a
+#       given path or any of its parent directories. If the path doesn't
+#       exist, the script will check one level down util it returns 0,
+#       indicating that the user has permission to write in the path,
+#       or 1, indicating that the user doesn't have write permission.
+#
+#=======================================================================
+# EXAMPLE
+#       hasWritePermission "/path/to/file"
+#       hasWritePermission "/path/to/folder"
+#       hasWritePermission "$HOME/path/to/folder"
+#       hasWritePermission "path/to/folder"
+#
+#=======================================================================
+# END_OF_HEADER
+#=======================================================================
+
+_checkWritePermission() {
+
+    local dir="${1}"
+
+    if [[ ! -w "${dir}" ]]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+hasWritePermission() {
+
+    if [[ "${#}" -ne 1 ]]; then
+        printf "hasWritePermission: missing required argument\n" >&2
+        printf "e.g, $ hasWritePermission path/to/file\n" >&2
+        exit 1
+    fi
+
+    local dir="${1}"
+    local parentDir
+
+    while true; do
+        if _checkWritePermission "${dir}"; then
+            return 0
+        fi
+
+        parentDir=$(dirname "${dir}")
+
+        if [[ "${parentDir}" == "${dir}" ]]; then
+            return 1
+        fi
+
+        dir="${parentDir}"
+    done
+}
